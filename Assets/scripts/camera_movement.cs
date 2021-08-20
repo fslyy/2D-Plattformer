51549022d467d07e7798fd2ce7387d79e4ac5efe
fslyy <88577347+fslyy@users.noreply.github.com>
2021-08-20 13:03:29 +0200

    more ground tiles + placeholder background

--- Assets/scripts/camera_movement.cs
+++ Assets/scripts/camera_movement.cs
@@ -7,7 +7,14 @@ public class camera_movement : MonoBehaviour
 
     public Transform player;
     public Vector3 offset;
+    private void Start()
+    {
+        float screenHeightInUnits = Camera.main.orthographicSize * 2;
+        float screenWidthInUnits = screenHeightInUnits * Screen.width / Screen.height;
 
+        Debug.Log(screenHeightInUnits);
+        Debug.Log(screenWidthInUnits);
+    }
     void Update()
     {
         transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z); 
